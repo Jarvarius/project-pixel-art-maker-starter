3c65b68edd8d00516907b8b1056a91a9cafa55fb
Hazard <ljarvarius@gmail.com>
2022-07-21 13:32:33 -0400

    var became let

--- designs.js
+++ designs.js
@@ -1,34 +1,30 @@
 // Select color input
 // Select size input
-var submit =  document.getElementById('sizePicker');
+let submit = document.getElementById("sizePicker");
 
-var table = document.getElementById('pixelCanvas');
+let table = document.getElementById("pixelCanvas");
 
-submit.addEventListener("submit",function(event){
-    event.preventDefault();
-    makeGrid();
+sizePicker.addEventListener("submit", function (event) {
+  event.preventDefault();
+  makeGrid();
 });
 
 // When size is submitted by the user, call makeGrid()
 
-
 function makeGrid() {
-    var height = document.getElementById('inputHeight').value;
-    var width = document.getElementById('inputWidth').value;
-
-    for (var v = 0; v < height; v++){
-        tr = document.createElement("tr");
-        for (var t = 0; t < width; t++){
-             td = document.createElement("td");
-             tr.insertAdjacentElement("afterbegin",td);
-            td.addEventListener("click",function(event){
-                
-                        var colorvalue = document.getElementById("colorPicker").value;
-                        event.target.style.backgroundColor = colorvalue;
-                
-            });
-        }
-        table.insertAdjacentElement("afterbegin",tr);
+  let height = document.getElementById("inputHeight").value;
+  let width = document.getElementById("inputWidth").value;
+   
+  for (var v = 0; v < height; v++) {
+    tr = document.createElement("tr");
+    for (var t = 0; t < width; t++) {
+      td = document.createElement("td");
+      tr.insertAdjacentElement("afterbegin", td);
+      td.addEventListener("click", function (event) {
+        let colorvalue = document.getElementById("colorPicker").value;
+        event.target.style.backgroundColor = colorvalue;
+      });
     }
-
+    table.insertAdjacentElement("afterbegin", tr);
+  }
 }
